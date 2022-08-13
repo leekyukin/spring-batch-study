9897ca82ca8b705ac19d7c61c0f9ec209004c838
leekyukin <leekujin14@gmail.com>
2022-08-14 01:07:11 +0900

    ADD :: Chunk 단위 multi thread

---
+++ batch-test/src/main/java/com/jik/batchtest/job/parallel/AmountFieldSetMapper.java
@@ -0,0 +1,17 @@
+package com.jik.batchtest.job.parallel;
+
+import com.jik.batchtest.dto.AmountDto;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public class AmountFieldSetMapper implements FieldSetMapper<AmountDto> {
+    @Override
+    public AmountDto mapFieldSet(FieldSet fieldSet) throws BindException {
+        return AmountDto.builder()
+                .index(fieldSet.readInt(0))
+                .name(fieldSet.readString(1))
+                .amount(fieldSet.readInt(2))
+                .build();
+    }
+}
