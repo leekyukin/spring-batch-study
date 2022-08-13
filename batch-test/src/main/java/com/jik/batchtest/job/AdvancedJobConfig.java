fd31548b72b1be86f493dad665153e7311dc1af8
leekyukin <leekujin14@gmail.com>
2022-08-13 17:38:35 +0900

    ADD :: AdvancedJob with LocalDateParameterValidator

---
+++ batch-test/src/main/java/com/jik/batchtest/job/AdvancedJobConfig.java
@@ -0,0 +1,56 @@
+package com.jik.batchtest.job;
+
+import com.jik.batchtest.job.validator.LocalDateParameterValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class AdvancedJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job advancedJob(Step advancedStep) {
+        return jobBuilderFactory.get("advancedJob")
+                .incrementer(new RunIdIncrementer())
+                .validator(new LocalDateParameterValidator("targetDate"))
+                .start(advancedStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step advancedStep(Tasklet advancedTasklet) {
+        return stepBuilderFactory.get("advancedStep")
+                .tasklet(advancedTasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet advancedTasklet(@Value("#{jobParameters['targetDate']}") String targetDate) {
+        return (contribution, chunkContext) -> {
+            log.info("[AdvancedJobConfig] JobParameter targetDate = " + targetDate);
+            LocalDate.parse(targetDate);
+            log.info("[AdvancedJobConfig] executed advancedTasklet");
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
